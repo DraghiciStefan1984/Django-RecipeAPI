c7eb85389220b1d1b1b95ca243bb03c30143a592
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2026-04-26 08:21:29 +0300

    setup django admin

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,22 @@
 from django.db import models
-from django.contrib.auth.model import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -7,5 +24,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
+    
